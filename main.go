325c52122adc809bf990c62c3f8b54e8564b2cd4
haojie06 <whaojie06@gmail.com>
2024-08-06 16:14:06 +0800

    debug output

--- main.go
+++ main.go
@@ -265,20 +265,18 @@ func createSnapshotTasks(snapshotName, grafanaURL, dashboardId, query string, fr
 	return chromedp.Tasks{
 		chromedp.Navigate(fmt.Sprintf("%s/d/%s/?from=%d&to=%d&%s", grafanaURL, dashboardId, from, to, query)),
 		logAction("wait for dashboard loaded"),
+		logAction("wait for dashboard loaded"),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			var screenshot []byte
-			var htmlContent string
 			if err := chromedp.Run(ctx, chromedp.ActionFunc(func(ctx context.Context) error {
 				if err := chromedp.Run(ctx,
 					chromedp.Sleep(5*time.Second),
-					chromedp.OuterHTML("html", &htmlContent),
 					chromedp.FullScreenshot(&screenshot, 100)); err != nil {
 					return err
 				}
 				if err := os.WriteFile("screenshot.png", screenshot, 0644); err != nil {
 					zap.S().Errorf("write screenshot")
 				}
-				zap.S().Debugf("html content: %s", htmlContent)
 				return nil
 			})); err != nil {
 				return err
